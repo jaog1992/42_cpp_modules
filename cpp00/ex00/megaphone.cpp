f9dbb817de8aa86a049948c867234acfa64c6a28
jaog1992 <jde-orma@IC3205.caf.local>
2024-08-17 15:25:51 +0200

    cpp00 ex00 update

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jde-orma <jde-orma@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/16 12:57:25 by jde-orma          #+#    #+#             */
+/*   Updated: 2024/08/17 15:08:47 by jde-orma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main (int argc, char **argv)
+{
+    if (argc == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    for(int i = 1; i < argc; i++)
+        for(int j = 0; argv[i][j] != '\0'; j++)
+            std::cout << static_cast<char>(std::toupper(argv[i][j]));
+    return(std::cout << std::endl, EXIT_SUCCESS);
+}
